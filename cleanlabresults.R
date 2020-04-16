85b564242995f22b5ae66d3bce36a1d5393f39f5
Travis <wvp3@cdc.gov>
2020-04-16 13:19:53 -0400

    Create cleanlabresults.R
    Clean up first batch of seroprevalence lab sample results

---
+++ cleanlabresults.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(data.table)
+library(readxl)
+
+labresults <- read_excel("20200416incomlpete dataset for COVIDTF.xlsx") %>%
+dplyr::rename_all(tolower) %>%
+dplyr::rename(zip=`zip code`) %>%
+dplyr::mutate(agegroup = case_when(
+  age %in% 0:4 ~ "0-4",
+  age %in% 5:17 ~ "5-17",
+  age %in% 18:49 ~ "18-49",
+  age %in% 50:64 ~ "50-64",
+  age %in% 65:79 ~ "65-79",
+  age >= 80 ~ "80+")) %>%
+dplyr::mutate(zip=stringr::str_sub(zip,1,5)) %>%
+dplyr::mutate(sex=case_when(
+  sex %in% c("M","m") ~ "Male",
+  sex %in% c("F","f") ~ "Female",
+  TRUE ~ "Unknown"
+)) %>%
+dplyr::filter(state %in% c("NY","WA"))
+
+write.csv(labresults,file="labresults_4-16-2020.csv",row.names=F)
